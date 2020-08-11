f3d970425232788277863c97d89aaae9528b7dcb
Greg Hartwell <hartwell@physics.auburn.edu>
2020-08-11 14:34:20 -0500

    Update dBdt_vs_r.py

--- gregsprograms/dBdt_vs_r.py
+++ gregsprograms/dBdt_vs_r.py
@@ -14,22 +14,24 @@ import numpy as np
 
 
 def dBdt_vs_r(shot,fwidth,idx):
-    r_positions=[1.6125,2.2313]
-    BrHF=[7.000E-3,6.134E-4]
-    BpHF=[2.528E-3,2.644E-4]
-    BzHF=[-7.424E-3,-1.528E-3]
     
-    BrTVF=[2.548e-3,-4.704e-4]
-    BpTVF=[-1.35e-6,-5.457e-7]
-    BzTVF=[4.849e-3,5.736e-3]
+    #define positions and components for 1000A unit current fields
+    r_positions=[1.5437,1.8,2.2]
+    BrHF=[9.892e-3,2.971e-3,6.771e-4]
+    BpHF=[3.351e-3,1.195e-3,2.921e-4]
+    BzHF=[-9.362E-3,-4.418E-3,1.161e-3]
     
-    BrTF=[8.083e-3,1.738e-4]
-    BpTF=[7.902e-4,3.956e-6]
-    BzTF=[8.922e-4,9.53e-6]
+    BrTVF=[3.996e-3,3.36e-4,-4.737e-4]
+    BpTVF=[-1.518e-6,-9.987e-7,-5.681e-7]
+    BzTVF=[2.261e-3,7.424e-3,5.918e-3]
     
-    BrOH=[2.974E-7,1.715e-5]
-    BpOH=[-5.174e-7,-2.078e-7]
-    BzOH=[-3.291e-4,-1.969e-4]
+    BrTF=[1.349e-2,2.177e-3,2.044e-4]
+    BpTF=[1.968e-3,8.751e-5,4.611e-6]
+    BzTF=[1.594e-3,2.051e-4,1.193e-5]
+    
+    BrOH=[-5.852e-5,1.775e-5,1.769e-5]
+    BpOH=[-5.174e-7,-3.826e-7,-2.163e-7]
+    BzOH=[-3.259e-4,-3.009e-4,-2.036e-4]
     
     # get the currents
     
@@ -43,28 +45,34 @@ def dBdt_vs_r(shot,fwidth,idx):
     TF.get_data(shotnum=shot,node='\\I_TF')
     OH.get_data(shotnum=shot,node='\\I_OH')
     
+    HFdata=HF.data[0:47999]
+    TVFdata=TVF.data[0:47999]
+    TFdata=TF.data[0:47999]
     
-    # need to downsample the OH array
+    # need to upsample the HF, TVF, TF arraya
     
-    HFdata = signal.resample(HF.data,len(OH.data))
-    TVFdata = signal.resample(TVF.data,len(OH.data))
-    TFdata = signal.resample(TF.data,len(OH.data))
+    HFdata = signal.resample(HFdata,len(OH.data))
+    TVFdata = signal.resample(TVFdata,len(OH.data))
+    TFdata = signal.resample(TFdata,len(OH.data))
     
+    #Convert to unit currents in 
     HFdata=np.array(HFdata)/1000.0
     TVFdata=np.array(TVFdata)/1000.0
     TFdata=np.array(TFdata)/1000.0
     OHdata=np.array(OH.data)/1000.0
     taxis=OH.taxis
     
-    plt.plot(taxis,HFdata)
-    plt.plot(taxis,TVFdata)
-    plt.plot(taxis,TFdata)
-    plt.plot(taxis,OHdata)
-    plt.show()
+    # these plot statements confirm that arrays have same time bais
+    # plt.plot(taxis,HFdata)
+    # plt.plot(taxis,TVFdata)
+    # plt.plot(taxis,TFdata)
     
-    plt.xlim(xmin,xmax)
-    plt.title('dBmod/dt')
-    plt.show()
+    # plt.plot(taxis,OHdata)
+    # plt.show()
+    
+    # plt.xlim(xmin,xmax)
+    # plt.title('dBmod/dt')
+    # plt.show()
 
     print('position =',r_positions[idx])
     Br = BrHF[idx]*HFdata + BrTVF[idx]*TVFdata \
@@ -77,12 +85,22 @@ def dBdt_vs_r(shot,fwidth,idx):
 
   
     if fwidth >=5:
-        Br=signal.savgol_filter(Br, fwidth, 3) # window size fwidth, polynomial order 3
+        # window size fwidth, polynomial order 3
+        Br=signal.savgol_filter(Br, fwidth, 3) 
         Bp=signal.savgol_filter(Bp, fwidth, 3)
         Bz=signal.savgol_filter(Bz, fwidth, 3)
     dBrdt=np.gradient(Br) /np.gradient(taxis)
     dBpdt=np.gradient(Bp)/np.gradient(taxis)
     dBzdt=np.gradient(Bz)/np.gradient(taxis)
+
+    dBrdt=np.gradient(Br)/1.0E-5
+    dBpdt=np.gradient(Bp)/1.0E-5
+    dBzdt=np.gradient(Bz)/1.0E-5
+
+    # plt.plot(taxis,np.gradient(taxis))
+    # plt.title('gradient')
+    # plt.show()
+    # print('length',len(taxis))
 
     if fwidth >=5:
         dBrdt=signal.savgol_filter(dBrdt, fwidth, 3)
@@ -90,37 +108,37 @@ def dBdt_vs_r(shot,fwidth,idx):
         dBzdt=signal.savgol_filter(dBzdt, fwidth, 3)
     xmin=0.0
     xmax=2.0
-    print('max Br',max(dBrdt))
-    print('max Bp',max(dBpdt))
-    print('max Bz',max(dBzdt))
+    print('max d(Br)/dt',max(dBrdt))
+    print('max d(Bp)/dt',max(dBpdt))
+    print('max d(Bz)/dt',max(dBzdt))
     
-    maxBr=max(dBrdt)
-    maxBp=max(dBpdt)
-    maxBz=max(dBzdt)
+    # maxBr=max(dBrdt)
+    # maxBp=max(dBpdt)
+    # maxBz=max(dBzdt)
     
     dbmod=np.sqrt(dBrdt**2+dBpdt**2+dBzdt**2)
-    print('max bmod 2',max(dbmod))
+    print('max d(bmod)/dt',max(dbmod))
     
-    plt.plot(taxis,dbmod)
-    plt.xlim(xmin,xmax)
-    plt.title('dBmod/dt')
-    plt.show()
+    # plt.plot(taxis,dbmod)
+    # plt.xlim(xmin,xmax)
+    # plt.title('dBmod/dt')
+    # plt.show()
     
-    print('max dBmod',np.sqrt(maxBr**2+maxBp**2+maxBz**2))
-    plt.plot(taxis,dBrdt)
-    plt.xlim(xmin,xmax)
-    plt.title("dBr/dt all coils")
-    plt.show()
+    # print('max dBmod',np.sqrt(maxBr**2+maxBp**2+maxBz**2))
+    # plt.plot(taxis,dBrdt)
+    # plt.xlim(xmin,xmax)
+    # plt.title("dBr/dt all coils")
+    # plt.show()
     
-    plt.plot(taxis,dBpdt)
-    plt.title("dBp/dt all coils")
-    plt.xlim(xmin,xmax)
-    plt.show()
+    # plt.plot(taxis,dBpdt)
+    # plt.title("dBp/dt all coils")
+    # plt.xlim(xmin,xmax)
+    # plt.show()
     
-    plt.plot(taxis,dBzdt)
-    plt.title("dBz/dt all coils")
-    plt.xlim(xmin,xmax)
-    plt.show()
+    # plt.plot(taxis,dBzdt)
+    # plt.title("dBz/dt all coils")
+    # plt.xlim(xmin,xmax)
+    # plt.show()
     
     # ## OH only data
     # OHdata=np.array(OH.data)/1000.0
